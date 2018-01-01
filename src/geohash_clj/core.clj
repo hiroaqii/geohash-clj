39f1bb161d0d645a1eef0d31d54bb34d6922ba8e
hiroaqii <hiroaqii@gmail.com>
2018-01-02 02:38:21 +0900

    Add geohash->base10

--- src/geohash_clj/core.clj
+++ src/geohash_clj/core.clj
@@ -1,11 +1,13 @@
 (ns geohash-clj.core)
 
-(def base32 "0123456789bcdefghjkmnpqrstuvwxyz")
+(def base32 (zipmap "0123456789bcdefghjkmnpqrstuvwxyz" (range)))
+
+(defn geohash->base10 [geohash]
+  (when (every? #(base32 %) geohash)
+    (->> geohash
+         (map #(base32 %))
+         (reduce #(bit-or (bit-shift-left %1 5) %2)))))
 
 (defn encode [lat lon])
 
 (defn decode [geohash])
-
-
-
-
