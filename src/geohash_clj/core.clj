22bbbe1f9002700f6800ee947610cbd24f23c29e
hiroaqii <hiroaqii@gmail.com>
2018-01-03 23:23:40 +0900

    Add separate function

--- src/geohash_clj/core.clj
+++ src/geohash_clj/core.clj
@@ -8,6 +8,15 @@
          (map #(base32 %))
          (reduce #(bit-or (bit-shift-left %1 5) %2)))))
 
+(defn separate [n]
+  (loop [x 0 y 0 i 0]
+    (if (> i 60)
+      [x y]
+      (recur
+       (if (bit-test n (inc i))(bit-set x (/ i 2)) x)
+       (if (bit-test n i)(bit-set y (/ i 2)) y)
+       (+ i 2)))))
+
 (defn encode [lat lon])
 
 (defn decode [geohash])
