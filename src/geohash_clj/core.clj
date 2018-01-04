3701303f43eb3bfb897db4f7b260acf589831331
hiroaqii <hiroaqii@gmail.com>
2018-01-04 20:50:15 +0900

    Add decode function

--- src/geohash_clj/core.clj
+++ src/geohash_clj/core.clj
@@ -2,21 +2,37 @@
 
 (def base32 (zipmap "0123456789bcdefghjkmnpqrstuvwxyz" (range)))
 
-(defn geohash->base10 [geohash]
-  (when (every? #(base32 %) geohash)
-    (->> geohash
-         (map #(base32 %))
-         (reduce #(bit-or (bit-shift-left %1 5) %2)))))
 
-(defn separate [n]
-  (loop [x 0 y 0 i 0]
-    (if (> i 60)
-      [x y]
-      (recur
-       (if (bit-test n (inc i))(bit-set x (/ i 2)) x)
-       (if (bit-test n i)(bit-set y (/ i 2)) y)
-       (+ i 2)))))
+(defn zero-pad [s]
+  (let [n (- 5 (count s))
+        col (map #(- (int %) 48) s)]
+    (concat (repeat n 0) col)))
+
+
+(defn geohash->bits [geohash]
+  (when (every? #(base32 %) geohash)
+    (mapcat
+     #(->> (base32 %)
+           (Integer/toBinaryString)
+           (zero-pad))
+     geohash)))
+
+
+(defn separate [s]
+  (let [cols (partition 2 s)]
+    (list (map first cols) (map last cols))))
+
+
+(defn locate [[min-loc max-loc] col]
+  (let [mid ( / (+ min-loc max-loc) 2)]
+    (if (empty? col)
+      [(float min-loc) (float max-loc)]
+      (recur (if (zero? (first col)) [min-loc mid] [mid max-loc]) (rest col)))))
+
 
 (defn encode [lat lon])
 
-(defn decode [geohash])
+
+(defn decode [geohash]
+  (let [[lat lon] (separate (geohash->bits geohash))]
+   [(locate [-180 180] lat) (locate [-90 90] lon)]))
